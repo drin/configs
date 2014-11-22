fab8dd716a72b01dea9a18645dd1834da7d4a118
Aldrin Montana <drinmontana@gmail.com>
2014-11-22 13:59:41 -0800

    Fixed my really silly hardcoded bash script to a much less silly, less hardcoded perl script

--- vim/plugins/plugins_from_git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/bling/vim-airline.git
-git clone https://github.com/derekwyatt/vim-scala.git
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/Shougo/neocomplete.vim.git
-git clone https://github.com/tomasr/molokai.git
-git clone https://github.com/wting/rust.vim.git
