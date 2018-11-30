97c008e90bd116154cf71c8107145a7c9496ed45
Aldrin <Drin@users.noreply.github.com>
2018-11-29 19:28:46 -0800

    changed ssh-agent variables from universal variables to global variables, which won't last across terminals but it will also prevent weirdness across reboots.

--- fish/functions/launch_ssh_agent.fish
+++ fish/functions/launch_ssh_agent.fish
@@ -10,12 +10,17 @@ function launch_ssh_agent
    else
       echo "ssh agent already running."
 
-      if not set -q SSH_AGENT_PID
-         set -Ux SSH_AGENT_PID (pgrep ssh-agent)
+      if set -q SSH_AGENT_PID
+          echo "clearing SSH_AGENT_PID: $SSH_AGENT_PID"
+          set -ge SSH_AGENT_PID
       end
 
-      if not set -q SSH_AUTH_SOCK
-         set -Ux SSH_AUTH_SOCK (find /tmp -name 'agent.[[:digit:]]*' -type s)
+      if set -q SSH_AUTH_SOCK
+          echo "clearing SSH_AUTH_SOCK: $SSH_AUTH_SOCK"
+          set -ge SSH_AUTH_SOCK
       end
+
+     set -gx SSH_AGENT_PID (pgrep ssh-agent)
+     set -gx SSH_AUTH_SOCK (find /tmp -name 'agent.[[:digit:]]*' -type s)
    end
 end
