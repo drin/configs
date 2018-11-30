d256b2872916e4defdcd2cfbec90baf7664ee873
Aldrin <Drin@users.noreply.github.com>
2018-11-29 19:28:15 -0800

    adding env variables for poetry and cargo bin paths

--- fish/env.fish
+++ fish/env.fish
@@ -12,3 +12,8 @@ end
 if not set -q __fish_color_blue
    set -gx __fish_color_blue (set_color -o blue)
 end
+
+# ------------------------------
+# Env Variables
+set -gx PATH $HOME/.poetry/bin $PATH
+set -gx PATH $HOME/.cargo/bin $PATH
