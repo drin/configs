c5af56360992226ea408c24a4c2697af6c021ac6
Aldrin <Drin@users.noreply.github.com>
2019-04-08 12:59:31 -0700

    changed the launch_ssh_agent fish function to check if the output of pgrep ssh-agent is empty, if not then clear ssh-agent environment variables and set them to appropriate ones. checking the environment variables is an incorrect approach.
    also, updated subsequent tests (for clearing ssh-agent env variables) to
    quote the variables in case multiple ssh-agents are running

--- fish/functions/launch_ssh_agent.fish
+++ fish/functions/launch_ssh_agent.fish
@@ -2,7 +2,7 @@
 # Function to launch ssh agent and properly export SSH AGENT environment
 # variables via a python script
 function launch_ssh_agent
-    if test -z $SSH_AGENT_PID
+    if test -z (pgrep ssh-agent)
         echo "Launching new ssh agent..."
 
         eval (ssh-agent -c | sed 's/^setenv/set -gx/')
@@ -10,12 +10,12 @@ function launch_ssh_agent
     else
         echo "ssh agent already running."
 
-        if test -n $SSH_AGENT_PID
+        if test -n "$SSH_AGENT_PID"
             echo "clearing SSH_AGENT_PID: $SSH_AGENT_PID"
             set -ge SSH_AGENT_PID
         end
 
-        if test -n $SSH_AUTH_SOCK
+        if test -n "$SSH_AUTH_SOCK"
             echo "clearing SSH_AUTH_SOCK: $SSH_AUTH_SOCK"
             set -ge SSH_AUTH_SOCK
         end
