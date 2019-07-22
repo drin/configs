493f9d036366c972ba7daeaa958dfbb58217a34b
Aldrin <Drin@users.noreply.github.com>
2019-07-22 12:18:10 -0700

    updating fish environment because it had an annoying error message on some servers. Specifically, removing init_fish function, moving useful contents to env.fish and wrapping env variables in an if block that checks if a specific env variable has been set.
    also, added a convenience alias for ucsc vpn on linux

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,5 @@
-if status --is-login
-   init_fish
-else if not set -q __fish_configured
-   init_fish
-end
-
 set fish_files $FISH_CONFIG_HOME/env.fish $FISH_CONFIG_HOME/aliases.fish
+
 for fish_file in $fish_files
    if test -e $fish_file
       echo "=> Loading $fish_file"
