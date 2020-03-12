f0023fdaf55111434f6590568c117b22838b8ea5
akmontan <akmontan@client0.singlecell.skyhook-pg0.wisc.cloudlab.us>
2020-03-12 15:52:26 -0500

    Adding script to make installing git on centos 7 easier, in case I stick with it for skyhook dev

---
+++ installers/install-git.centos.sh
@@ -0,0 +1,23 @@
+sudo yum install libcurl-devel expat-devel asciidoc xmlto docbook2X
+
+mkdir -p ${HOME}/installers
+mkdir -p ${HOME}/toolbox/git
+
+pushd ${HOME}/installers
+if [ ! -d "git-2.25.1" ]; then
+    echo "Downloading and extracting git source..."
+
+    wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.25.1.tar.xz
+    tar -xzf git-2.25.1.tar.xz
+else
+    echo "Using existing git source..."
+fi
+
+# Note: after building, will need to modify Makefile to use old name of
+# docbook2X binary
+# Reference: https://linuxfollies.blogspot.com/2015/11/docbook2x-texi.html
+cd git-2.25.1/
+make prefix=${HOME}/toolbox/git all doc
+make prefix=${HOME}/toolbox/git install install-doc
+
+popd
