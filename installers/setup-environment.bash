b5df21d13993571c69762e0c7d6ff9acf27c9569
akmontan <akmontan@ucsc.edu>
2020-04-02 11:19:52 -0500

    Added command to soft link $HOME/.terminfo -> $CONFIG_ROOT/terminfo

--- installers/setup-environment.bash
+++ installers/setup-environment.bash
@@ -20,9 +20,11 @@ path_to_home_config="${HOME}/.config"
 path_to_configs_bash="${CONFIG_ROOT}/bash"
 path_to_configs_fish="${CONFIG_ROOT}/fish"
 path_to_configs_alacritty="${CONFIG_ROOT}/alacritty"
+path_to_configs_terminfo="${CONFIG_ROOT}/terminfo"
 
 path_to_home_fish="${path_to_home_config}/fish"
 path_to_home_alacritty="${path_to_home_config}/alacritty"
+path_to_home_terminfo="${HOME}/.terminfo"
 
 
 # ------------------------------
@@ -58,14 +60,21 @@ fi
 
 # ------------------------------
 # Alacritty setup
+
+# Install alacritty package
 if [[ -f /etc/os-release && -n $(grep "Ubuntu" /etc/os-release) ]]; then
     # Assume yes, so that this doesn't wait for me to press enter
     sudo add-apt-repository -y ppa:mmstick76/alacritty
     sudo apt-get -y install alacritty
 fi
 
+# Link alacritty's configuration
 if [[ -d ${path_to_home_config} && ! -e ${path_to_home_alacritty} ]]; then
     ln -s ${path_to_configs_alacritty} ${path_to_home_alacritty}
 else
     echo "Config home does not exist; or alacritty link already exists. Skipping..."
 fi
+
+# Link terminfo files
+# (necessary for ssh to ubuntu for alacritty for some reason)
+ln -s ${path_to_configs_terminfo} ${path_to_home_terminfo}
