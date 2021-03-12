2302cbbafc45d2d092513f21acaea3fb8e946ad1
Aldrin <akmontan@ucsc.edu>
2021-03-12 11:25:38 -0800

    aliases.fish: added echo for arch

--- archlinux/fish/aliases.fish
+++ archlinux/fish/aliases.fish
@@ -3,3 +3,8 @@
 alias vim         'vim -O'
 alias external-ip 'dig @resolver1.opendns.com ANY myip.opendns.com +short'
 alias vpn-ucsc    'sudo openconnect --verbose 128.114.102.44:443'
+
+
+# ------------------------------
+# Signal that this file has been invoked
+echo "-- Fish aliases initialized"
