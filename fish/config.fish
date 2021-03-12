a8ebb2a20a03baf7d05746e8e05aa7fa825e81f0
Aldrin <akmontan@ucsc.edu>
2021-03-12 10:30:25 -0800

    config.fish: Removed a useless check

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,13 @@
-if test -e $HOME/.config/fish
-    set -Ux FISH_CONFIG_HOME $HOME/.config/fish
-end
+# Only do this if the session is interactive
+if status --is-interactive
+    if test -e $HOME/.config/fish
+        set fish_files $HOME/.config/fish/env.fish $HOME/.config/fish/aliases.fish
 
-
-if set -q "FISH_CONFIG_HOME"
-    set fish_files $FISH_CONFIG_HOME/env.fish $FISH_CONFIG_HOME/aliases.fish
-
-    for fish_file in $fish_files
-       if test -e $fish_file
-          echo "=> Loading $fish_file"
-          source $fish_file
-       end
+        for fish_file in $fish_files
+           if test -e $fish_file
+              echo "=> Loading $fish_file"
+              source $fish_file
+           end
+        end
     end
 end
-
