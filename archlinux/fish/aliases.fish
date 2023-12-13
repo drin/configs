54613378366f556f7f9b93aedbf3dc12c995455e
Aldrin Montana <octalene.dev@pm.me>
2023-12-12 16:41:08 -0800

    mostly minor changes and a few new things
    minor changes are mostly whitespace or style, new things involve a few
    more convenience aliases for more quickly changing monitor config

--- archlinux/fish/aliases.fish
+++ archlinux/fish/aliases.fish
@@ -1,11 +1,17 @@
 #!/usr/bin/fish
 
-alias vim         'vim -O'
-alias external-ip 'dig @resolver1.opendns.com ANY myip.opendns.com +short'
-alias vpn-ucsc    'sudo openconnect --verbose vpn.ucsc.edu'
-alias vdown       'pactl set-sink-volume (pactl list short sinks | grep bluez_sink* | cut -f1) -5%'
-alias vup         'pactl set-sink-volume (pactl list short sinks | grep bluez_sink* | cut -f1) +5%'
-alias vcheck      'pactl get-sink-volume (pactl list short sinks | grep bluez_sink* | cut -f1)'
+alias vim           'vim -O'
+alias external-ip   'dig @resolver1.opendns.com ANY myip.opendns.com +short'
+alias vpn-ucsc      'sudo openconnect --verbose vpn.ucsc.edu'
+alias vdown         'pactl set-sink-volume (pactl list short sinks | grep "bluez_sink.30_50_75_" | cut -f1) -5%'
+alias vup           'pactl set-sink-volume (pactl list short sinks | grep "bluez_sink.30_50_75_" | cut -f1) +5%'
+alias vcheck        'pactl get-sink-volume (pactl list short sinks | grep "bluez_sink.30_50_75_" | cut -f1)'
+
+alias set-mon-dual  'xrandr --output HDMI-A-0      --auto --primary --output DisplayPort-1 --left-of HDMI-A-0 --auto'
+alias set-mon-right 'xrandr --output HDMI-A-0      --auto --primary --output DisplayPort-1 --off'
+alias set-mon-left  'xrandr --output DisplayPort-1 --auto --primary --output HDMI-A-0      --off'
+
+alias set-wallpapers 'feh --bg-max "$primary_wp" "$secondary_wp"'
 
 
 # ------------------------------
