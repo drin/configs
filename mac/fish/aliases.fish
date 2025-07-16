8f3be472987fea022f7a321262ed7a894d406e28
Aldrin M <aldrin.montana@bauplanlabs.com>
2025-07-16 09:36:39 -0700

    task: minor updates

--- mac/fish/aliases.fish
+++ mac/fish/aliases.fish
@@ -1,12 +1,13 @@
 #!/usr/bin/env fish
 
+# >> Common aliases
 alias vim         'vim -O'
 alias external-ip 'dig @resolver1.opendns.com ANY myip.opendns.com +short'
-alias vpn-ucsc    'sudo openconnect --verbose https://vpn.ucsc.edu/'
 
-# because fish is installed by brew, and thus architecture dependent
-alias amdfish     'arch -x86_64 /usr/local/bin/fish'
-alias armfish     'arch -arm64  /opt/homebrew/bin/fish'
+# >> Architecture dependent aliases
+# fish is arch dependent if rosetta homebrew is used
+# alias amdfish 'arch -x86_64 /usr/local/bin/fish'
+# alias armfish 'arch -arm64  /opt/homebrew/bin/fish'
 
 
 # ------------------------------
